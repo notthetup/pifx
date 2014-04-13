32077174d835b245062c0c1bcaa219d0d35d9e21
Chinmay Pendharkar <notthetup@gmail.com>
2014-04-13 17:28:07 +0800

    moved to alsa

--- initPiFx.sh
+++ initPiFx.sh
@@ -3,17 +3,10 @@
 # Kill mumble if it's already running
 pkill mumble
 
-#Kill pulseaudio if already running
-if [ `pgrep pulseaudio` ]; then pulseaudio -k; fi
-
-# Init Pulseaudio
-DISPLAY=:0.0 pulseaudio --high-priority=1 & 
-
-#Wait for Pulse to be init
-until [ `pgrep pulseaudio` ]; do sleep 1; done;
-
 # A bit more delay to stabilize things
 sleep 2;
 
 # Start Mumble
 DISPLAY=:0.0 mumble "mumble://pifx@listen.webuild.sg/webuildsg-live?&version=1.2.0" &
+
+#aplay -D hw:0,1 /usr/share/sounds/alsa/Front_Left.wav
